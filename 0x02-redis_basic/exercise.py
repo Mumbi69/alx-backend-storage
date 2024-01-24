add8ce3b730425e8d771d5f0939309a01533bafb
Mumbi69 <winfriendmumbi@gmail.com>
2024-01-25 01:44:14 +0300

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -43,10 +43,13 @@ class Cache:
 if __name__ == "__main__":
     cache = Cache()
 
-    data = b"hello"
-    key = cache.store(data)
-    print(key)
 
-    local_redis = redis.Redis()
-    print(local_redis.get(key))
+    TEST_CASES = {
+        b"foo": None,
+        123: int,
+        "bar": lambda d: d.decode("utf-8")
+    }
 
+    for value, fn in TEST_CASES.items():
+        key = cache.store(value)
+        assert cache.get(key, fn=fn) == value
